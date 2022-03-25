36eb72d5ba329751d905c79f73e23134429bade5
Julie <jawarren89@gmail.com>
2022-03-25 14:24:59 -0700

    Refined function d

--- function_d.py
+++ function_d.py
@@ -3,13 +3,8 @@ def max_value(numbers):
         in the list.
     """
     max_number = max(numbers)
-    print(max_number)
     return max_number
 
 
-# if __name__ == "__main__":
-#     print(max_value([1, 12, 2, 42, 8, 3]))
-
-
-numbers = [4, 6, 56]
-max_value(numbers)
+if __name__ == "__main__":
+    print(max_value([1, 12, 2, 42, 8, 3]))
